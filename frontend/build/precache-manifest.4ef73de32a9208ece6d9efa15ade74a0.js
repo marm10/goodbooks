482fb3460955862a28e9386fb03353fcd5b4f1f6
marm10 <marcilio.ec.ufc@gmail.com>
2020-10-22 09:18:21 -0300

    fim

---
+++ frontend/build/precache-manifest.4ef73de32a9208ece6d9efa15ade74a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94386f6c80dd161376e1c2967d2036d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c8abbd4e0e4b6dc5629",
+    "url": "/static/css/2.ffbcb6fa.chunk.css"
+  },
+  {
+    "revision": "4c8abbd4e0e4b6dc5629",
+    "url": "/static/js/2.10e82a68.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.10e82a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fb5a3f8b8a61c2c5e3",
+    "url": "/static/js/main.c9f28c52.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
